3e4f5a335076f9787da39ec1f7e983068098f6a9
Georgiana Haldeman <ghaldeman@colgate.edu>
2023-12-08 10:40:41 -0500

    sample code

--- src/test/java/project/system/RetrieveTODOsFlowTest.java
+++ src/test/java/project/system/RetrieveTODOsFlowTest.java
@@ -1,7 +1,24 @@
 package project.system;
 
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.safari.SafariDriver;
+
+import project.system.pages.HomeWebPage;
 
 public class RetrieveTODOsFlowTest {
+    protected static WebDriver driver = new SafariDriver();
+    private HomeWebPage page = new HomeWebPage(driver);
     
+    @Test 
+    void retrieveTodoMultipleTest(){
+        //page.retrieveTodos("tony-stark");
+        page.visit();
+        page.retrieveTodos("tony-stark");
+        //driver.get("http://localhost:8080");
+        page.isReady();
+        System.out.println(page.retrieveTodos());
+        System.out.println("!!!!!!!!!!!!!!!!!!!!!!!! end of test");
+    }
 
 }
